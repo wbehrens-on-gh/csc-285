fa0ae7d752b9370ff786e03bdc79383a0dabcf30
Delaney Lentz <delaneyjlentz@gmail.com>
2023-11-07 15:17:43 -0500

    work on project2

--- src/Project2.cpp
+++ src/Project2.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
+#include <tuple>
+#include <vector>
+using namespace std;
 
-int main() { return 0; }
+#define GRAPH std::vector<std::tuple<int, int>>
+
+bool is_bipartite(GRAPH g) {
+    vector<int> team1;
+    vector<int> team2;
+
+    for (int i=0; i<=g.size(); i++){
+        if(){
+          team1.push_back(get<0>(line));
+        } else{
+            team2.push_back(get<1>(line));
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    is_bipartite({{0,1}
+    });
+}
+
