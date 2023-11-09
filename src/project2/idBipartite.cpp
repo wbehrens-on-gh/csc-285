62f157de8274f3ee53025f4951a6638f06e1511e
William Behrens <will@behr.codes>
2023-11-09 13:17:51 -0600

    create project dir

---
+++ src/project2/idBipartite.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+using namespace std;
+
+#define GRAPH std::vector<std::tuple<int, int>>
+
+// Delaney
+/*
+bool is_bipartite(GRAPH g) {
+  vector<int> team1;
+  vector<int> team2;
+
+  for (int i = 0; i <= g.size(); i++) {
+    if () {
+      team1.push_back(get<0>(line));
+    } else {
+      team2.push_back(get<1>(line));
+    }
+  }
+
+  return true;
+}
+*/
+
+int main() {}
