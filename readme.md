72d330a7d5348091f3211c88f44c1036ce8b2164
William Behrens <will@behr.codes>
2023-11-10 15:26:09 -0600

    maybe finish q4?

--- readme.md
+++ readme.md
@@ -5,3 +5,8 @@ Code for Applied Discrete Structures (csc-285).
 ## Building
 
 To build and run this code use `go build <path/to/file.go>`. The programs you can execute can be found in the [src/cmd](src/cmd) directory.
+
+## D2CC
+
+D$'s 2 Cents converter is a program written in Go to convert from binary TO base 10 and base 16 then convert to binary FROM base 10 and base 16. 
+It includes a basic "tui" menu for selecting the operation and not much else. 
--- src/project2/computationsAndExplorations4.cpp
+++ src/project2/computationsAndExplorations4.cpp
@@ -1,46 +1,71 @@
 #include <iostream>
-#include <sys/wait.h>
 #include <vector>
 
-#define NUM_VERT 10
-#define GRAPH std::vector<std::vector<int>>
-
-/* A graph will look something like this
- *
- *    0 1 2 3 4 5
- * 0 [0 1 0 1 0 1]
- * 1 [0 1 1 0 0 0]
- * 2 [0 1 0 0 1 0]
- * 3 [0 1 0 1 0 1]
- * 4 [0 1 0 1 1 1]
- * 5 [0 1 1 0 1 1]
- */
+#define NUM_VERT 20
+#define GRAPH std::vector<std::vector<uint>>
 
 using namespace std;
 
+/*
+ * Define simple:
+ * - no loops: each column in the matrix can have at most n-1 connections where
+ * n is the number of vertices.
+ * - no contradiction: if a point is conneted to n-1 points then all points
+ * should at least be connected to 1 other point
+ * - connections must agree: if a connects to b then b is connected to a
+ * - A simple graph can be connected or disconnected
+ *   mathworld.wolfram.com/simplegraph.html
+ */
+
+/**
+ * A graph will look something like this when printed in a terminal
+ *
+ *    0 1 2 3 4 5
+ * 0 [0 1 1 1 1 1]
+ * 1 [1 0 1 1 1 1]
+ * 2 [1 1 0 1 1 1]
+ * 3 [1 1 1 0 1 1]
+ * 4 [1 1 1 1 0 1]
+ * 5 [1 1 1 1 1 0]
+ */
 void printGraph(GRAPH g) {
   cout << "  ";
-  for (int i = 0; i < NUM_VERT; i++) {
+  for (size_t i = 0; i < NUM_VERT; i++) {
     cout << " " << i;
   }
   cout << endl;
 
-  for (int i = 0; i < NUM_VERT; i++) {
+  for (size_t i = 0; i < NUM_VERT; i++) {
     cout << i << " [";
-    for (int j = 0; j < NUM_VERT; j++) {
-      cout << g.at(i).at(j) << " ";
+    for (size_t j = 0; j < NUM_VERT; j++) {
+      cout << g[i][j] << " ";
     }
     cout << "]" << endl;
   }
 }
 
-int main() {
-  GRAPH result(NUM_VERT);
-  for (int i = 0; i < NUM_VERT; i++) {
-    for (int j = 0; j < NUM_VERT; j++) {
-      result.at(i).push_back(rand() % 2);
+/**
+ * Generate at random a simple graph with n vertices.
+ */
+GRAPH generateGraph(uint n) {
+  GRAPH result(n, vector<uint>(n, 0));
+  for (size_t row = 0; row < NUM_VERT; row++) {
+    for (size_t col = 0; col < NUM_VERT; col++) {
+      uint connected = 0;
+      if (row != col) { // ensure no loops
+        connected = rand() % 2;
+        if (connected == 1) { // undirected: (a,b) = 1 -> (b,a) = 1
+          result.at(row).at(col) = connected;
+        }
+      }
+      result[row][col] = connected;
     }
   }
-  printGraph(result);
+  return result;
+}
+
+int main() {
+  GRAPH graph = generateGraph(NUM_VERT);
+  printGraph(graph);
   return 0;
 }
